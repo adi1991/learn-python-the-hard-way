201cb05ecd8d1b00ac6d2870a2fa1aa3bf56208b
Aditya <{reachadit91@gmail.com}>
2015-06-02 09:48:55 -0400

    my first commit

---
+++ ex1.py
@@ -0,0 +1,37 @@
+print "Hellow World"
+print "Hello again"
+print "Whats your name??"
+print "this is so much fun" 
+
+
+
+
+#Extra code 
+
+print "hello world"
+print 4.0 * 4.0
+print 4 * 4
+
+myName = 'Aditya'
+myLastName = 'Seshan'
+myAge = 24 # years
+myWeight = 180 # in pounds
+
+print "Hey my full name is", myName, myLastName
+print "My age is", myAge
+
+#while True:
+	#for i in ["/","-","|","\\","|"]:
+		#print "%s\r" % i,
+print "What is your name",
+name = raw_input()
+print "what is your age",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, your name is %r, You are %r old, your height is %r and you weigh %r." % (name, age, height, weight)
+# learn-python-the-hard-way
+# learn-python-the-hard-way
